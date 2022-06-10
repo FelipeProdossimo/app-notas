6297d919bc8701646d8f520eeca7755624815cff
Felipe Prodóssimo <73621753+FelipeProdossimo@users.noreply.github.com>
2022-06-09 22:22:36 -0300

    Aplicando APIs

--- appnotas/www/js/app.js
+++ appnotas/www/js/app.js
@@ -2,10 +2,13 @@ function SalvarAnotacao(){
     novaAnotacao = document.getElementById("anotacao").value
     document.getElementById("anotacao").value = "";
     document.getElementById("anotacoes-salvas").innerHTML += "<p>" + novaAnotacao + "</p>"
-    document.getElementById("anotacoes").value = ""
+    document.getElementById("anotacao").value = ""
     alert("Anotacao salva com sucesso")
 }
 
 document.getElementById("botao-salvar").addEventListener("click", function(){
     SalvarAnotacao()
+    //navigator.vibrate(250);
+    navigator.vibrate(350);
+    console.log("teste")
 })

7b3715d7c1b62efd05228ec042c71687f9b30278
Felipe Prodóssimo <73621753+FelipeProdossimo@users.noreply.github.com>
2022-06-22 22:29:58 -0300

    App atualizado!

--- appnotas/www/js/app.js
+++ appnotas/www/js/app.js
@@ -1,8 +1,13 @@
 function SalvarAnotacao(){
     novaAnotacao = document.getElementById("anotacao").value
-    document.getElementById("anotacao").value = "";
-    document.getElementById("anotacoes-salvas").innerHTML += "<p>" + novaAnotacao + "</p>"
-    document.getElementById("anotacao").value = ""
+    novoNome = document.getElementById("nome").value
+    novoTel = document.getElementById("tel").value
+
+    novoContato = "<p><h1 class='text-danger'>Anotado: </h1>" + novaAnotacao + "<br><h1>Nome: </h1>" + novoNome + "<br><h1>Telefone: </h1>" + novoTel +"</p>"
+    document.getElementById("anotacoes-salvas").innerHTML = novoContato
+
+    localStorage.listaContatos = novoContato
+    
     alert("Anotacao salva com sucesso")
 }
 
@@ -12,3 +17,16 @@ document.getElementById("botao-salvar").addEventListener("click", function(){
     navigator.vibrate(350);
     console.log("teste")
 })
+
+if(localStorage.listaContatos) {
+    document.getElementById("anotacoes-salvas").innerHTML = localStorage.listaContatos
+}
+
+function clearcontent(elementID) {
+    document.getElementById(elementID).innerHTML = "";
+    localStorage.clear()
+}
+
+function excluir(e){
+    e.parentElement.remove();
+}
